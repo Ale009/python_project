d6f09528e076e1730f6048d2f4b8e5b4758732ee
“Asalazar” <salazarale9@hotmail.com>
2025-06-07 11:52:48 -0600

    Se agrega la la URI par pruebas locales

--- frontend/javascript/uri.js
+++ frontend/javascript/uri.js
@@ -1,1 +1,2 @@
-export const URI = 'https://api-flask-render-l6he.onrender.com'
+//export const URI = 'https://api-flask-render-l6he.onrender.com'
+export const URI = 'http://localhost:5000' // Uncomment this line to use the local Flask server
